5978b3bc7f434592b9ee512dd9717bc5dd8db144
GianLost <gianluca19993m@gmail.com>
2024-02-18 00:41:13 -0300

    Inicializando o repositório

---
+++ Dominando_Os_Operadores/Gerenciamento_De_Tarefas/Program.cs
@@ -0,0 +1,33 @@
+﻿//  DESCRIÇÃO :
+// Agora, focaremos em outro conceito fundamental de programação para desenvolver um algoritmo que possua como objetivo a simulação de um Sistema de Gerenciamento de Tarefas. Dessa forma, crie variáveis como, titulo, descricao e dataVencimento para guardar as informações das tarefas, após isso, implemente uma estrutura condicional 'if/else' para verificar se a descrição da tarefa excede 50 caracteres. Caso a descrição exceda 50 carateres, trataremos essa condição exibindo uma mensagem adequada. Lembrando que as estruturas condicionais são fundamentais para o controle de fluxo dos programas pois realizam verificações precisas com base em condições específicas predefinidas, neste caso, é o limite de 50 caracteres.
+
+// ENTRADA :
+// A entrada será as Strings com o titulo da tarefa, a descrição e a data de vencimento.
+
+// SAÍDA :
+// Será exibido a tarefa que foi adicionada com o título, descrição e data de vencimento. Mas caso a descrição tenha mais de 50 caracteres deverá ser informado: 'O Valor da descrica excede a quantidade de caracteres permitido'.
+
+string Titulo;
+string Descricao;
+string DataDeVencimento;
+
+// Obtém o titulo e a descricao a partir da entrada do console
+Console.WriteLine("Informe um titulo para a sua tarefa :");
+Titulo = Console.ReadLine();
+
+Console.WriteLine("Informe uma descrição para a sua tarefa :");
+Descricao = Console.ReadLine();
+
+// TODO: Crie uma estrutura condicional 'if/else' para verificar se a descrição da tarefa excede 50 caracteres.
+if (!(Descricao.Length > 50))
+{
+    // Caso a descricao esteja dentro do limite é solicitado a entrada pelo console:
+    Console.WriteLine("Informe uma data de vencimento para a sua tarefa :");
+    DataDeVencimento = Console.ReadLine();
+    // TODO: Imprima a descricao e a data de vencimento como nos exemplos da tabela:
+    Console.WriteLine($"{Descricao} ate {DataDeVencimento}");
+}
+else
+{
+    Console.WriteLine("Descricao ultrapassa limite de caracteres.");
+}
---
+++ Explorando_Sintaxe_E_Tipos_De_Dados/Registro_De_Usuario/Program.cs
@@ -0,0 +1,30 @@
+﻿// DESCRIÇÃO :
+// Neste desafio, aplicaremos conceitos essenciais de programação, seu objetivo é desenvolver uma solução simulando o registro de usuário em uma aplicação. Assegure que o código tenha uma sintaxe clara e organizada, com nomenclaturas significativas para variáveis como email, nomeUsuario, senha que será o local de armazenamento das informações de registro. Escolha tipos de dados apropriados, como o String para representar o email, nomeUsuario e senha. Certifique-se de tratar senha como strings para operações específicas.
+
+// ENTRADA :
+// Como entrada receberemos três informações referente ao emailCadastro(String), nomeUsuario(String) e senha(String).
+
+// SAÍDA :
+// Construa uma mensagem de saída formatada corretamente, como nessa estrutura: ' registroNome + ", verifique o email: " + registroEmail + " para ativar." '
+
+// TODO: Declare as variáveis para guardar as informações de nome, email e senha:
+string Email;
+string NomeUsuario;
+string Senha;
+
+// Obtém o email e nome do usuário a partir da entrada do console
+Console.WriteLine("Informe o seu nome :");
+string registroNome = Console.ReadLine();
+
+Console.WriteLine("Informe o seu email :");
+string registroEmail = Console.ReadLine();
+
+Console.WriteLine("Informe a sua senha :");
+string registroSenha = Console.ReadLine();
+
+Email = registroEmail;
+NomeUsuario = registroNome;
+Senha = registroSenha;
+
+// TODO: Imprima a mensagem formatada com o nome do usuário e o email de registro:
+Console.WriteLine($"{NomeUsuario} verifique o email: {Email} para ativar.");
