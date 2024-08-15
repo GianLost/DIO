2ebb8c3bc307d129635c3cac1847b23e6beb37a5
GianLost <gianluca19993m@gmail.com>
2024-08-15 13:33:10 -0300

    Adicionando novo desafio.

---
+++ Desafios_De_Codigo/Otimizacao_De_Alocacoes_De_Ativos_Com_Restricoes/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Recebe a entrada do número de ativos
+        int N = int.Parse(Console.ReadLine());
+        
+        // Receben e divide os valores de mercado em um array de strings
+        string[] valoresMercadoStr = Console.ReadLine().Split(',');
+        
+        double[] valoresMercado = Array.ConvertAll(valoresMercadoStr, double.Parse);
+        
+        // Recebe o valor total investido
+        double valorTotalInvestido = double.Parse(Console.ReadLine());
+        
+        // Recebe e divide as alocações mínimas em um array de strings
+        string[] alocacoesMinimasStr = Console.ReadLine().Split(',');
+        
+        double[] alocacoesMinimas = Array.ConvertAll(alocacoesMinimasStr, double.Parse);
+        
+        // Recebendo e dividindo as alocações máximas em um array de strings
+        string[] alocacoesMaximasStr = Console.ReadLine().Split(',');
+        
+        double[] alocacoesMaximas = Array.ConvertAll(alocacoesMaximasStr, double.Parse);
+        
+        // Calcula o total do mercado
+        double totalMercado = 0;
+        
+        for (int i = 0; i < N; i++)
+        {
+            // TODO: Some os valores de mercado para obter o total
+            totalMercado += valoresMercado[i];
+        }
+
+        // Calcula as alocações proporcionais e ajustando aos limites mínimos e máximos
+        double[] alocacoes = new double[N];
+        
+        for (int i = 0; i < N; i++)
+        {
+            // TODO: Calcule a alocação proporcional
+            double proporcional = valoresMercado[i] / totalMercado * valorTotalInvestido;
+            
+            alocacoes[i] = Math.Max(alocacoesMinimas[i], Math.Min(alocacoesMaximas[i], proporcional)); // Ajusta dentro dos limites mínimos e máximos
+        }
+
+        // Imprime as alocações formatadas com duas casas decimais
+        for (int i = 0; i < N; i++)
+        {
+            Console.WriteLine($"{alocacoes[i]:F2}"); // Mostra cada alocação formatada com duas casas decimais
+        }
+    }
+}
