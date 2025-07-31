b3db9518236650f621cd0d92a7899c698dfb3bea
GianLost <gianluca19993m@gmail.com>
2025-07-30 21:49:38 -0300

    Adicionando .gitignore e um novo desafio.

---
+++ Desafios_De_Codigo/Calculo_De_Frete_Rapido/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Digite o peso em quilos:");
+double peso = Convert.ToDouble(Console.ReadLine());
+
+// TODO: Leia o valor do frete por quilo
+Console.WriteLine("Digite o valor do frete por quilo:");
+double frete = Convert.ToDouble(Console.ReadLine());
+
+// TODO: Calcule o valor total do frete
+double valorTotalFrete = peso * frete;
+
+// TODO: Exiba o valor formatado com duas casas decimais
+Console.WriteLine($"{valorTotalFrete:F2}");
